48eda5f160d8268e3dab460389f38fc24e47a39d
ZinoChan <devzinochan@gmail.com>
2023-02-21 02:32:48 -0800

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-	int s = 0;
-	
-	while ( s < 10)
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-		 print_alphabet();
-		 s++;
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 	}
 }
