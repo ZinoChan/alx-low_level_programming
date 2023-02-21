87185a96f0e030689db399775938341f0614f638
ZinoChan <devzinochan@gmail.com>
2023-02-21 02:30:15 -0800

    task-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: prints alphabetsx10
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int s = 0;
+	
+	while ( s < 10)
+	{
+		 print_alphabet();
+		 s++;
+	}
+}
