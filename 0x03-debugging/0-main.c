e47d321b284f0500b83bf8e5f2b7b3e7c9510c87
ZinoChan <devzinochan@gmail.com>
2023-02-22 02:07:20 -0800

    task-0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: test function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = -7 ;
+	positive_or_negative(i);
+
+	return (0);
+}
