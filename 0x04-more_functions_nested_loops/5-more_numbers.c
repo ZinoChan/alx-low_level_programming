e5ca8ba69c4cb32624d6ffbb72c53a31b6e68b2a
ZinoChan <devzinochan@gmail.com>
2023-02-23 07:21:39 -0800

    another fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, c;
 
 	i = 0;
 
 	while (i < 10)
 	{
-		for (int c = 0; c <= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
 			if (c >= 10)
 				_putchar((c / 10) + '0');
