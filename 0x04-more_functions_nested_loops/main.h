29f44ca79ad9f0716411ba7c09b5e1c0d7a5e33c
ZinoChan <devzinochan@gmail.com>
2023-02-23 06:56:33 -0800

    more tasks

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,7 @@ void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
 #endif
