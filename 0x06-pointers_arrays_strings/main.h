96520fb4a7984963ac0e16984e6a4501fb1e7952
ZinoChan <devzinochan@gmail.com>
2023-03-02 07:31:47 -0800

    task 11 & 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
