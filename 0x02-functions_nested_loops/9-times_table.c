f8716b2a64158acf51108cdf6a50db61723c71ec
ZinoChan <devzinochan@gmail.com>
2023-03-03 05:21:05 -0800

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,10 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j < 10; j++)
 		{
 			res = i * j;
 			if (res < 10)
