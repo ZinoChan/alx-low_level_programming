fc2a23e10785fa322cc8dac42522800a40796ea7
ZinoChan <devzinochan@gmail.com>
2023-03-03 06:41:16 -0800

    edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,36 +7,40 @@
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long int fib1, fib2, fib, x, y;
+	unsigned long int carry1, carry2;
+	int i;
 
-	j = 1;
-	k = 2;
+	fib1 = 1;
+	fib2 = 2;
+	x = 0;
+	y = 0;
 
-	printf("%lu", j);
+	printf("%lu, %lu, ", fib1, fib2);
 
-	for (i = 1; i < 91; i++)
+	for (i = 2; i < 98; i++)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		if(fib1 + fib2 > 10000000000 || x > 0 || y > 0)
+		{
+			carry1 = (fib1 + fib2) / 10000000000;
+			fib = (fib1 + fib2) % 10000000000;
+			carry2 = x + y + carry1;
+			x = y;
+			y = carry2;
+			fib1 = fib2;
+			fib2 = fib;
+			printf("%lu%010lu", y, fib2);
+		}
+		else
+		{
+			fib = fib1 + fib2;
+			fib1 = fib2;
+			fib2 = fib;
+			printf("%lu", fib2);
+		}
+		if(i != 97)
+			printf(" ,");
 	}
-
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
-	}
-
 	printf("\n");
-
-	return (0);
+	return(0);
 }
