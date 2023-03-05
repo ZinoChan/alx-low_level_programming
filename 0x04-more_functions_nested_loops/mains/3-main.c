ca69f9bda466973d9dab8d9d0d0d055a8b24590d
ZinoChan <devzinochan@gmail.com>
2023-03-05 22:36:38 +0100

    update

---
+++ 0x04-more_functions_nested_loops/mains/3-main.c
@@ -0,0 +1,18 @@
+#include "../main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("Expected value:\n");
+    printf("===\n");
+    printf("0123456789\n");
+    printf("===\n");
+    printf("Your Output:\n");
+    print_numbers();
+    return (0);
+}
--- 0x04-more_functions_nested_loops/mains/README.md
+++ 0x04-more_functions_nested_loops/mains/README.md
@@ -11,3 +11,10 @@ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 mains/0-main.c 0-isupper.c -o 0-i
 ```
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 mains/1-main.c 1-isdigit.c -o 1-isdigit
 ```
+
+- 2-mul.c
+
+```
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 mains/2-main.c 2-mul.c -o 2-mul
+
+```
