82a7b69f176661cb7ee45247536822c3e826c678
ZinoChan <devzinochan@gmail.com>
2023-03-06 06:43:00 -0800

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
 #endif
 
