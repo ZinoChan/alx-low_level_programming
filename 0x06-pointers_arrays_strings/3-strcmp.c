1a220f2d9e29940f0b1f534474d069630e9b7f8c
ZinoChan <devzinochan@gmail.com>
2023-03-07 11:15:58 -0800

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, c;
 
-	for (i = 0; s1[i] != '\0' && s1[i] == s2[i]; i++)
-		;
-	if (s1[i] == s2[i])
-		return (0);
-	return ((*s1 + 0) - (*s2 + 0));
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		c = (*s1 + 0) - (*s2 + 0);
+	}
+	return (c);
 }
