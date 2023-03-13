7a1f973abe97cef309957c009e55f3be61c56917
ZinoChan <devzinochan@gmail.com>
2023-03-13 00:16:24 -0700

    betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,17 @@
 
 char *create_array(unsigned int size, char c)
 {
-        char *arr;
+	char *arr;
 	unsigned int i;
 
-        if (size == 0)
-                return (NULL);
-        arr = malloc(size * sizeof(c));
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(c));
 
-        if (arr == NULL)
-                return (NULL);
+	if (arr == NULL)
+		return (NULL);
 
-        for (i = 0; i < size; i++)
-                arr[i] = c;
-        return (arr);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
 }
