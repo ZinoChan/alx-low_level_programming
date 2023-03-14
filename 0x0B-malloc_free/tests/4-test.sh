5b51ad714ba90980e1c3fe47924ef727c6b51920
ZinoChan <devzinochan@gmail.com>
2023-03-13 17:17:53 -0700

    test-4

---
+++ 0x0B-malloc_free/tests/4-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Expected value:"
+echo "==5013== Memcheck, a memory error detector
+==5013== Copyright (C) 2002-2015, and GNU GPL'd, by Julian Seward et al.
+==5013== Using Valgrind-3.11.0 and LibVEX; rerun with -h for copyright info
+==5013== Command: ./f
+==5013== 
+0 0 0 0 0 0 
+0 0 0 0 0 0 
+0 0 0 0 0 0 
+0 0 0 0 0 0 
+
+0 0 0 98 0 0 
+0 0 0 0 0 0 
+0 0 0 0 0 0 
+0 0 0 0 402 0 
+==5013== 
+==5013== HEAP SUMMARY:
+==5013==     in use at exit: 0 bytes in 0 blocks
+==5013==   total heap usage: 6 allocs, 6 frees, 1,248 bytes allocated
+==5013== 
+==5013== All heap blocks were freed -- no leaks are possible
+==5013== 
+==5013== For counts of detected and suppressed errors, rerun with: -v
+==5013== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)"
+
+if gcc -Wall -pedantic -Werror -Wextra -std=gnu89 mains/4-main.c 3-alloc_grid.c 4-free_grid.c -o f; then
+    echo -e "\n"
+    echo "Your output: "
+    valgrind ./f
+else
+    echo "Compilation failed"
+fi
