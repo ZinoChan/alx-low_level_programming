483359696ddf5872652a3b5f0f55831ab6b67293
ZinoChan <devzinochan@gmail.com>
2023-03-20 11:15:23 +0000

    fix: reference correct file name in gcc command

--- 0x0D-preprocessor/tests/2-test.sh
+++ 0x0D-preprocessor/tests/2-test.sh
@@ -3,7 +3,7 @@
 echo "Expected value:"
 echo "2-main.c"
 
-if gcc -Wall -pedantic -Werror -Wextra -std=gnu89 mains/2-main.c -o c; then
+if gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c -o c; then
     echo -e "\n"
     echo "Your output: "
     ./c
