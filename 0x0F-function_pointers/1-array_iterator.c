d4971c0e0b8502855d15c6f95e22015d68c812f4
ZinoChan <devzinochan@gmail.com>
2023-03-22 09:02:52 +0000

    betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if(array && action)
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
