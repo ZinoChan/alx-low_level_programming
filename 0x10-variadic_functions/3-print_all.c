6fda6968181ca27b24a9a7212ab2fb28961ef5bb
ZinoChan <devzinochan@gmail.com>
2023-03-23 09:39:03 +0000

    refactor there is not much information abt way the test fails so I'm assuming that the reason is the usage of switch statement so I decided to remove it

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,96 @@
-#include "variadic_functions.h"
 /**
- * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- *
- * Return: void
- */
-
+  * print_all - Prints anything
+  * @format: The conversion specifier to prints
+  *
+  * Return: Nothing
+  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int idx = 0, is_valid = 0;
-	char *s;
+	f_dt form_types[] = {
+		{ "c", print_a_char },
+		{ "i", print_a_integer },
+		{ "f", print_a_float },
+		{ "s", print_a_char_ptr }
+	};
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
 
 	va_start(args, format);
-	while (format[idx])
+
+	while (format != NULL && format[i])
 	{
-		switch (format[idx])
+		j = 0;
+		while (j < 4)
 		{
-			case 'c':
-				is_valid = 1;
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				is_valid = 1;
-				printf("%i", va_arg(args, int));
-				break;
-			case 'f':
-				is_valid = 1;
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				is_valid = 1;
-				s = va_arg(args, char *);
-				if (!s)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", s);
-				break;
+			if (format[i] == *form_types[j].identifier)
+			{
+				form_types[j].f(separator, args);
+				separator = ", ";
+			}
+			j++;
 		}
-		if (is_valid == 1 && format[idx + 1] != '\0')
-			printf(", ");
-		is_valid = 0;
-		idx++;
+		i++;
 	}
 
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
 
+/**
+  * print_a_char - Prints a character of char type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+/**
+  * print_a_integer - Prints a character of integer type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_integer(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+}
+
+/**
+  * print_a_float - Prints a character of float type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+/**
+  * print_a_char_ptr - Prints the content of pointer to char type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_char_ptr(char *separator, va_list args)
+{
+	char *arg = va_arg(args, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, arg);
+}
 
