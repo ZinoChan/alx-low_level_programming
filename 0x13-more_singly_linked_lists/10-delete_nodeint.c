b84925c891a20f17a23b9b0a14d03d5452065224
ZinoChan <devzinochan@gmail.com>
2023-04-05 10:55:33 +0000

    fix NULL case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int idx = 0;
 	listint_t *curr, *prev;
 
-	if (head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	curr = *head;
