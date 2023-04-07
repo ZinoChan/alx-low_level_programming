6d6a6f5a8f7f7180e74df10c4669886674eea9b2
ZinoChan <devzinochan@gmail.com>
2023-04-07 18:44:13 +0000

    Add binary_to_uint function to convert binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: pointer to str of 0 and 1 chars
+ * Return: converted number or NULL
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bango = 0;
+	int hajimeru = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[hajimeru] != '\0')
+	{
+		if (b[hajimeru] == '0')
+			bango = bango * 2;
+		else if (b[hajimeru] == '1')
+			bango = bango * 2 + 1;
+		else
+			return (0);
+	}
+
+	return (bango);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
