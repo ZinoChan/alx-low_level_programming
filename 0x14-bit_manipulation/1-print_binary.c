c64c29c4a9526599f26179266599be6d187dd702
ZinoChan <devzinochan@gmail.com>
2023-04-07 19:16:22 +0000

    minor fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,11 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) ? '1' : 0);
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
 }
