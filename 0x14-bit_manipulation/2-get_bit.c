b2ccddac90d5dce80307c42d3c19b189a8576c56
ZinoChan <devzinochan@gmail.com>
2023-04-08 22:10:54 +0000

    Add get_bit function implementation to return the value of bit at the given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * get_bit - gives value of bit at idx
+ * @index: index to give value
+ * @n: number
+ * Return: value at idx or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_per_byto = 8;
+	unsigned int bits_in_longo = sizeof(n) * bit_per_byto;
+
+	if (index >= bits_in_longo)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
