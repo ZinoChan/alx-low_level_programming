ac4c89d4ba8ba3164f05387aee29f1fc42a50f80
ZinoChan <devzinochan@gmail.com>
2023-04-08 22:54:58 +0000

    Add function to detect endianness in main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 void recursive_print(unsigned long int n);
+int get_endianness(void);
 
 #endif
