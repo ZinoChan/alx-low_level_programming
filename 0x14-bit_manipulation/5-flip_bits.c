a898c75c2a4b9cd6bc2bd28c7bf00e399f786cb9
ZinoChan <devzinochan@gmail.com>
2023-04-08 22:33:48 +0000

    Add function flip_bits that calculates the number of bits to flip to get from n to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * flip_bits - num of bits to flip to get from n to another
+ * @n: oh baby how I' suppose to know
+ * @m: I must confess that I still believe
+ * Return: hit me baby one more time
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int hit_me_baby_one_more_time = 0;
+	int give_me_a_sign = n ^ m;
+
+	while (give_me_a_sign)
+	{
+		hit_me_baby_one_more_time += (give_me_a_sign & 1);
+		give_me_a_sign >>= 1;
+	}
+
+	return (hit_me_baby_one_more_time);
+
+}
