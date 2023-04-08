8d1e678ef4874f9ef53e4a414bc217d28555cc9d
ZinoChan <devzinochan@gmail.com>
2023-04-08 22:23:01 +0000

    Add function clear_bit to set value of bit to 0 at specified index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * clear_bit - sets val of bit to 0 at idx
+ * @index: the idx
+ * @n: pointer to bit
+ * Return: 1 or -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int bit_per_byto = 8;
+	int bit_per_longo = sizeof(unsigned long int) * bit_per_byto;
+
+	if (n == NULL || index >= bit_per_byto)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
