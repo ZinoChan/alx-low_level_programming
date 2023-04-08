1ea3415090f7da5fbf4b90d80df9828835051ca5
ZinoChan <devzinochan@gmail.com>
2023-04-08 21:34:39 +0000

    refactor

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	recursive_print(n);
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar('0' + (n & 1));
 }
 
-/**
- * recursive_print - prints binary of number
- * @n: number to convert
- * Return: binary of a number
-*/
-
-void recursive_print(unsigned long int n)
-{
-	if (n < 1)
-		return;
-	print_binary(n >> 1);
-	_putchar((n & 1) ? '1' : '0');
-}
