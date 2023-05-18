551f5dbc0a8d3a3a4eb522f82899b19fd601e936
ZinoChan <devzinochan@gmail.com>
2023-05-18 20:37:56 +0100

    handle null node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
