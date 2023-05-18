d96d06ca4bfceaabb951c25bfd12b06c1e4401e6
ZinoChan <devzinochan@gmail.com>
2023-05-18 21:08:03 +0100

    add null check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int curr = 0;
 	dlistint_t *ptr, *new_node;
 
+	if (h == NULL)
+		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	ptr = *h;
@@ -27,8 +30,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (ptr == NULL)
 		return (add_dnodeint_end(h, n));
-
-
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
