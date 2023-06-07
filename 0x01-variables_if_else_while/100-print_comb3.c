a151d4e1703e758a226fdad5aad0a54daf6d839a
ZinoChan <devzinochan@gmail.com>
2023-06-07 16:42:56 +0100

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
 			if (i < 8)
 			{
-				putchar(' ');
 				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
