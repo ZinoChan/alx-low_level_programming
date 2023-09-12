67265fa8af3b6c7a830bfe147cc3804a6e798986
ZinoChan <devzinochan@gmail.com>
2023-09-12 10:08:06 +0100

    init

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+#include <stddef.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+#endif
