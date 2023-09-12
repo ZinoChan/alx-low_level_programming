35d06d14af939a458c1730630e7006ad61a94bbf
ZinoChan <devzinochan@gmail.com>
2023-09-12 10:17:41 +0100

    fix and add readme

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
 #include <stddef.h>
+#include <stdio.h>
 
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
