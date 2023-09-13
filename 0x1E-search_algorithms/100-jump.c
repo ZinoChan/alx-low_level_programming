51c0f6a35f13497ca566de7cf03bdd37ba9fa445
ZinoChan <devzinochan@gmail.com>
2023-09-13 09:54:43 +0100

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@ size_t min(size_t a, size_t b)
 }
 
 /**
- * linear_search - Perform linear search on a subarray
+ * _linear_search - Perform linear search on a subarray
  * @array: Pointer to the first element of the subarray
  * @start: Starting index of subarray
  * @end: Ending index of subarray
@@ -21,9 +21,11 @@ size_t min(size_t a, size_t b)
  */
 int _linear_search(int *array, size_t start, size_t end, int value)
 {
-	for (size_t i = start; i < end; ++i)
+	size_t i;
+
+	for (i = start; i < end; ++i)
 	{
-		printf("%d\n", array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
@@ -39,21 +41,35 @@ int _linear_search(int *array, size_t start, size_t end, int value)
  */
 int jump_search(int *array, size_t size, int value)
 {
+	size_t block_size = sqrt(size);
+	size_t prev = 0;
+	int idx;
+
 	if (array == NULL)
 		return (-1);
 
-	size_t block_size = sqrt(size);
-	size_t prev = 0;
 
 	while (array[min(block_size, size) - 1] < value)
 	{
-		printf("%d\n", array[min(block_size, size) - 1]);
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev = block_size;
 		block_size += sqrt(size);
 		if (prev >= size)
 			return (-1);
 	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	prev, min(block_size, size) - 1);
 
-	return (_linear_search(array, prev, min(block_size, size), value));
+	idx = _linear_search(array, prev, min(block_size, size), value);
+	if (idx != -1)
+	{
+		printf("Found %d at index: %d\n", value, idx);
+		return (idx);
+	}
+	else
+	{
+		printf("Found %d at index: -1\n", value);
+		return (-1);
+	}
 }
 
